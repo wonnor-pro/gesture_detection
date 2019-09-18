ee8dbc5de6f7c99c1686e02400521a0f5c493a57
xw345 <xw345@cam.ac.uk>
2019-09-18 20:50:52 +0800

    cnn train added

--- inference.py
+++ inference.py
@@ -8,16 +8,10 @@ import matplotlib.pyplot as plt
 
 tf.enable_eager_execution()
 
-# dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
 dataset_path = "dataset_sorted/"
 testdata_amount = 500
 train_file_path_list, train_label_list, test_file_path_list, test_label_list = get_train_test_list(dataset_path, testdata_amount)
-train_dataset = load_dataset(train_file_path_list, train_label_list, batchsize=32, repeat=10)
 test_dataset = load_dataset(test_file_path_list, test_label_list, batchsize=32, repeat=10)
-
-print(train_dataset)
-
-plot = False
 
 
 class_names = ['0SingleOne', '1SingleTwo', '2SingleFour', '3SingleSix',
@@ -38,11 +32,13 @@ def normalize(images, labels):
 
 # The map function applies the normalize function to each element in the train
 # and test datasets
-train_dataset =  train_dataset.map(normalize)
+
 test_dataset  =  test_dataset.map(normalize)
 
-print(train_dataset)
+
 print(test_dataset)
+
+plot=True
 
 # Take a single image, and remove the color dimension by reshaping
 for image, label in test_dataset.take(1):
@@ -60,7 +56,7 @@ if plot:
 
     plt.figure(figsize=(10,10))
     i = 0
-    for (image, label) in test_dataset.shuffle(num_train_examples).take(25):
+    for (image, label) in test_dataset.shuffle(num_test_examples).take(25):
         image = image.numpy().reshape((28,28))
         plt.subplot(5,5,i+1)
         plt.xticks([])
@@ -76,21 +72,17 @@ if plot:
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
     tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dense(8,  activation=tf.nn.softmax),
+    tf.keras.layers.Dense(8,  activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
+model.load_weights('checkpoints/Sep18')
+
 BATCH_SIZE = 32
-train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
-test_dataset = test_dataset.batch(BATCH_SIZE)
-
-model.fit(train_dataset, epochs=50, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
-
-
-model.save_weights('checkpoints/Sep18')
+test_dataset = test_dataset.shuffle(num_test_examples).batch(BATCH_SIZE)
 
 
 # ---------------TESTING-----------------------
@@ -135,7 +127,7 @@ def plot_value_array(i, predictions_array, true_label):
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    thisplot = plt.bar(range(8), predictions_array, color="#777777")
     plt.ylim([0, 1])
     predicted_label = np.argmax(predictions_array)
 
@@ -144,16 +136,16 @@ def plot_value_array(i, predictions_array, true_label):
 
 
 
-# # Plot the first X test images, their predicted label, and the true label
-# # Color correct predictions in blue, incorrect predictions in red
-# num_rows = 5
-# num_cols = 3
-# num_images = num_rows*num_cols
-# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-# for i in range(num_images):
-#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
-#   plot_image(i, predictions, test_labels, test_images)
-#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
-#   plot_value_array(i, predictions, test_labels)
-# plt.show()
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 5
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
 
--- playground.py
+++ playground.py
@@ -1,8 +1,21 @@
-from utils import mkdir, scan_folder, scan_file, cropImg, drawBoxes
+import numpy as np
+import tensorflow as tf
 
-dataset_path = "label/"
-_, subfolder_list = scan_file(dataset_path, 'txt')
-subfolder_list.sort()
+def main(unused_argv):
+    # Load training and eval data
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images # Returns np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images # Returns np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+    print(train_data.shape)
+    print(train_data[0])
+    print(train_labels.shape)
+    print(train_labels)
+    print(eval_data.shape)
+    print(eval_labels.shape)
 
 
-print(subfolder_list)
+if __name__ == "__main__":
+    tf.app.run()
