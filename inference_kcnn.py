8fd055a5ded3db7f445d944838c07ea3dd8d804b
xw345 <xw345@cam.ac.uk>
2019-09-19 21:51:24 +0800

    data aug

--- inference_kcnn.py
+++ inference_kcnn.py
@@ -143,8 +143,8 @@ def plot_value_array(i, predictions_array, true_label):
 
 # Plot the first X test images, their predicted label, and the true label
 # Color correct predictions in blue, incorrect predictions in red
-num_rows = 5
-num_cols = 5
+num_rows = 2
+num_cols = 3
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
--- load_data.py
+++ load_data.py
@@ -2,6 +2,8 @@ from utils import mkdir, scan_folder, scan_file, cropImg, drawBoxes
 import os
 import numpy as np
 import cv2
+import random
+import time
 
 
 def validate(annotation_path = "annotation.txt"):
@@ -21,7 +23,7 @@ def validate(annotation_path = "annotation.txt"):
 
     return invalid
 
-def sort_data(label_path = "label/", dataset_path = "dataset_sorted/"):
+def sort_data(label_path = "label/", dataset_path = "dataset_sorted_aug/"):
 
     # Existing Paths
     # label_path
@@ -51,7 +53,10 @@ def sort_data(label_path = "label/", dataset_path = "dataset_sorted/"):
         subfolder_path = dataset_path + str(count)
         mkdir(subfolder_path)
 
+        deviation = [[1, 1], [1, -1], [-1, -1], [1, -1]]
         txt_path = label_path + txt_filename
+        MAX_X = 640
+        MAX_Y = 480
 
         with open(txt_path, 'rt') as f:
             print(txt_filename)
@@ -68,29 +73,67 @@ def sort_data(label_path = "label/", dataset_path = "dataset_sorted/"):
                     im = cv2.imread(img_path)
                     h = im.shape[0]
                     w = im.shape[1]
+                    random.seed(time)
+                    direction = random.choice(deviation)
+                    dx = random.randrange(1) * direction[0]
+                    dy = random.randrange(1) * direction[1]
                     x1 = int(float(words[1]) * w)
                     y1 = int(float(words[2]) * h)
                     x2 = int(float(words[3]) * w)
                     y2 = int(float(words[4]) * h)
-                    cropped_img = cropImg(im, x1, y1, x2, y2)
+                    _x1 = int(float(words[1]) * w - 0.1 * w + dx)
+                    _y1 = int(float(words[2]) * h - 0.1 * h + dy)
+                    _x2 = int(float(words[3]) * w + 0.1 * w + dx)
+                    _y2 = int(float(words[4]) * h + 0.1 * h + dy)
+                    if _x1<0:
+                        _x1 = 0
+                    elif _x1>MAX_X:
+                        _x1 = MAX_X
+                    if _x2<0:
+                        _x2 = 0
+                    elif _x2>MAX_X:
+                        _x2 = MAX_X
+                    if _y1<0:
+                        _y1 = 0
+                    elif _y1>MAX_Y:
+                        _y1 = MAX_Y
+                    if _y1<0:
+                        _y1 = 0
+                    elif _y1>MAX_Y:
+                        _y1 = MAX_Y
+                    cropped_img = cropImg(im, _x1, _y1, _x2, _y2)
                     resized_img = cv2.resize(cropped_img, (28, 28))
                     # print(resized_img.shape)
                     grayFrame = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+                    flipped = cv2.flip(grayFrame, 1)
                     # print(grayFrame.shape)
                     # cv2.imshow("gray", grayFrame)
                     # cv2.waitKey(0)
-                    new_img_path = "{}/{}.png".format(subfolder_path, count_line)
-                    cv2.imwrite(new_img_path, grayFrame)
-                    count_img[count] += 1
-                    single_record = "{} {}\n".format(new_img_path, count)
-                    print(single_record)
+                    # cv2.imshow("flip", flipped)
+                    # cv2.waitKey(0)
+                    new_img_path_1 = "{}/{}.png".format(subfolder_path, count_line)
+                    new_img_path_2 = "{}/{}.png".format(subfolder_path, 2*count_line)
+                    cv2.imwrite(new_img_path_1, grayFrame)
+                    cv2.imwrite(new_img_path_2, flipped)
+                    count_img[count] += 2
+                    single_record_1 = "{} {}\n".format(new_img_path_1, count)
+                    single_record_2 = "{} {}\n".format(new_img_path_2, count)
+                    print(single_record_1)
+                    print(single_record_2)
 
-                    isExists_new = os.path.exists(new_img_path)
+                    isExists_new = os.path.exists(new_img_path_1)
                     if isExists_new:
-                        record.append(single_record)
+                        record.append(single_record_1)
                         count_sample[count] += 1
                     else:
-                        print("new_img_path invalid")
+                        print("new_img_path_1 invalid")
+
+                    isExists_new = os.path.exists(new_img_path_2)
+                    if isExists_new:
+                        record.append(single_record_2)
+                        count_sample[count] += 1
+                    else:
+                        print("new_img_path_2 invalid")
 
                 else:
                     invalid_path.append(img_path)
